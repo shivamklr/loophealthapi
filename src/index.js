ea55a5a90bff66ddf1d2e5383245aa66dafcc882
Shivam Kalra <shivamkalra98@gmail.com>
2021-05-29 00:16:35 +0530

    Update index.js copy docs from mock api to db

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 const express = require("express");
+// const axios = require("axios");
 
-const config = require("./config/config.js")
+// const { createProduct } = require("./controllers/products.js");
+const config = require("./config/config.js");
 const db = require("./config/mongoose");
 const router = require("./routes");
-const PORT = process.env.PORT||config.PORT;
+const PORT = process.env.PORT || config.PORT;
 //express instance
 const app = express();
 //body parser
 app.use(express.json());
 // TODO: Add routes middleware
-app.use(router)
+app.use(router);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Listening for requests at port: http://localhost:${PORT}`);
 });
+// function to copy all values from mock api to mongodb cloud
+// axios("https://demo7242716.mockable.io/products").then((res) =>
+//     res.data.products.forEach(({productName, brand, gender, category, price, images}) =>
+//         createProduct({ body: {productName, brand, gender, category, price, images} },{})
+//     )
+// );
