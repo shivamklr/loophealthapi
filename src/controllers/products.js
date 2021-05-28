65569b50e98483c6facc128fa344d56e9d0c8203
Shivam Kalra <shivamkalra98@gmail.com>
2021-05-29 00:48:43 +0530

    Update products controller getFilters action

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,5 +1,6 @@
 const Product = require("../models/product");
 
+// FIXME: error handling missing
 module.exports.createProduct = async (req, res) => {
     const { productName, brand, gender, category, price, images } = req.body;
     // TODO: validate fields
@@ -36,8 +37,32 @@ module.exports.getProducts = async (req, res) => {
     }
     try {
         const products = await Product.find(filters).lean();
+        // TODO: add applied filters
         res.status(200).json({ data: { products } });
     } catch (e) {
+        console.log({ e });
+    }
+};
+
+module.exports.getFilters = async (req, res) => {
+    try {
+        let genders;
+        await Product.find({}).distinct("gender", (err, genderList) => {
+            if (err) throw new Error("Error while finding distinct gender");
+            genders = genderList;
+        });
+        let brands;
+        await Product.find({}).distinct("brand", (err, brandList) => {
+            if (err) throw new Error("Error while finding distinct brand");
+            brands = brandList;
+        });
+        let categories;
+        await Product.find({}).distinct("category", (err, categoryList) => {
+            if (err) throw new Error("Error while finding distinct category");
+            categories = categoryList;
+        });
+        res.status(200).json({ filters: { genders, brands, categories } });
+    } catch (error) {
         console.log({ e });
     }
 };
