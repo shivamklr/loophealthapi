f94a8a1ca9eced1a611f72749522580baca02cab
Shivam Kalra <shivamkalra98@gmail.com>
2021-05-28 21:06:38 +0530

    update index.js add router middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
 const config = require("./config/config.js")
-const app = express();
-app.use(express.json());
+const db = require("./config/mongoose");
+const router = require("./routes");
 const PORT = process.env.PORT||config.PORT;
-
+//express instance
+const app = express();
+//body parser
+app.use(express.json());
+// TODO: Add routes middleware
+app.use(router)
 
 app.listen(PORT, ()=>{
     console.log(`Listening for requests at port: http://localhost:${PORT}`);
