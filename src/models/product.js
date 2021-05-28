57a1518807012f466d165db92947b5987b50941e
Shivam Kalra <shivamkalra98@gmail.com>
2021-05-28 21:04:39 +0530

    add product model

---
+++ src/models/product.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    productName: {
+        type: String,
+        required: true,
+    },
+    brand: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    images: [{ view: String, src: String }],
+});
+module.exports = mongoose.model("Product", productSchema);
