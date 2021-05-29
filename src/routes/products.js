0b85556c90c298a46e16b151af5c9a38b2fe83d1
Shivam Kalra <shivamkalra98@gmail.com>
2021-05-29 22:18:12 +0530

    Add middleware products route

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { getProducts, getFilters } = require("../controllers/products");
+const { fetchValidator } = require("../middleware/validateQuery");
+const { validationCheck } = require("../middleware/validationPass");
 const router = express.Router();
 // GET /products
-router.get("/", getProducts);
+router.get("/", fetchValidator, validationCheck, getProducts);
 // GET /products/filters
 router.get("/filters", getFilters);
 module.exports = router;
